215b7ebe6f8b402929075387de92b818b37d647d
Brooke Strand <brooke.strand007@gmail.com>
2022-01-25 17:41:32 -0500

    commit tree 84e1d93b8a9f23e451db8c2d4589e518c6b87b36

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    if (record.result === "W") {
+        return record.year;
+    }
+    else {
+        return undefined;
+    }
+}
+let win = recordList.find(superbowlWin);
